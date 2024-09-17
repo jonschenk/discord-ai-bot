269e02c3de655980a73f019f66c757272a210762
jonschenk <jonschenkjr@gmail.com>
2024-09-17 14:36:57 -0700

    ollama works now

--- src/index.js
+++ src/index.js
@@ -1,36 +1,54 @@
-const { ollama } = require('Ollama');
+const {Ollama} = require('ollama')
+const ollama = new Ollama({
+    host: 'localhost:11434'
+})
 
-// Load environment variables
-require('dotenv').config();
+async function getResponse(message) {
+    const response = await ollama.chat({
+        model: 'llama3.1',
+        messages: [{ role: 'user', content: message }],
+    })
+    return await response.message.content
+}
 
-const { Client, IntentsBitField } = require('discord.js');
-const TOKEN = process.env.TOKEN;
-const PREFIX = process.env.PREFIX;
+async function main() {
+    const response = await getResponse('Hello');
+    console.log(response);
+}
 
-// Setting bot intents
-const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
-    ]
-});
+main();
 
-// On bot ready
-client.on('ready', (c) => {
-    console.log(`✅ ${c.user.username} is ready`);
-});
+// // Load environment variables
+// require('dotenv').config();
 
-// On each message create
-client.on('messageCreate', async (message) => {
-    if (message.author.bot) return;
+// const { Client, IntentsBitField } = require('discord.js');
+// const TOKEN = process.env.TOKEN;
+// const PREFIX = process.env.PREFIX;
 
-    if (message.content.startsWith(PREFIX)) {
-        const question = message.content.slice(PREFIX.length).trim();
-        console.log(`Received question: ${question}`);
+// // Setting bot intents
+// const client = new Client({
+//     intents: [
+//         IntentsBitField.Flags.Guilds,
+//         IntentsBitField.Flags.GuildMessages,
+//         IntentsBitField.Flags.MessageContent,
+//     ]
+// });
 
-        // TODO
-    }
-});
+// // On bot ready
+// client.on('ready', (c) => {
+//     console.log(`✅ ${c.user.username} is ready`);
+// });
 
-client.login(TOKEN);
+// // On each message create
+// client.on('messageCreate', async (message) => {
+//     if (message.author.bot) return;
+
+//     if (message.content.startsWith(PREFIX)) {
+//         const question = message.content.slice(PREFIX.length).trim();
+//         console.log(`Received question: ${question}`);
+
+//         // TODO
+//     }
+// });
+
+// client.login(TOKEN);
