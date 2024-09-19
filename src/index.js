e8f8535b5ee907b467d6c90c47974c4cf7239605
Jon Schenk <jonschenkjr@gmail.com>
2024-09-18 17:35:16 -0700

    messing with models

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ const client = new Client({
         IntentsBitField.Flags.Guilds,
         IntentsBitField.Flags.GuildMessages,
         IntentsBitField.Flags.MessageContent,
+        IntentsBitField.Flags.GuildMembers,
     ]
 });
 
@@ -50,6 +51,15 @@ client.on('messageCreate', async (message) => {
         pfp: message.author.displayAvatarURL()
     }
 
+    let server_members = []
+    // get all members in the guild
+    let current_server = message.guild;
+    for (let member of current_server.members.cache) {
+        server_members.push(member[1].user.username);
+    }
+
+    console.log(`Server members: ${server_members}`);
+
     // Prompt handling
     if (message.content.startsWith(PREFIX + "quote")) {
         quote = getRandomQuote();
@@ -60,17 +70,17 @@ client.on('messageCreate', async (message) => {
         console.log(`Received question: ${question}`);
         let reply_msg = message.reply("Generating response...");
         message.channel.sendTyping();
-        const response = await getResponse(question, current_user);
+        const response = await getResponse(question, current_user, server_members);
         console.log(`Response: ${response}`);
         (await reply_msg).edit(response);
     }
 });
 
 // Get the ollama response
-async function getResponse(message, user_info) {
+async function getResponse(message, user_info, server_members) {
     const response = await ollama.chat({
         model: 'llama3.1', // The model to use
-        messages: [{ role: 'user', content: `Your name is ${bot_name}, ${PROMPT} Their username is ${user_info.username}, Their nickname is ${user_info.nickname}, Their profile picture is located at this URL: ${user_info.pfp}. Here's their prompt, respond to it and do not reply or acknowledge to the previous information: ${message}`}],
+        messages: [{ role: 'user', content: `Your name is ${bot_name}, ${PROMPT} Their username is ${user_info.username}, Their nickname is ${user_info.nickname}, Their profile picture is located at this URL: ${user_info.pfp}. Also here's a list of the people in this server in case someone asks you a "who" question: ${server_members}. Here's their prompt, respond to it and do not reply or acknowledge to the previous information: ${message}`}],
     })
     return await response.message.content
 }
